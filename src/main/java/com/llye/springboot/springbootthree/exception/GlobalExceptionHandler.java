a2aee69d46e7d3c3f8522b943f1677af962ffe5c
joycelye <joycelye1991@gmail.com>
2023-07-31 00:11:35 +0800

    use record to reduce boilerplate code

--- src/main/java/com/llye/springboot/springbootthree/exception/GlobalExceptionHandler.java
+++ src/main/java/com/llye/springboot/springbootthree/exception/GlobalExceptionHandler.java
@@ -1,6 +1,5 @@
 package com.llye.springboot.springbootthree.exception;
 
-import lombok.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -13,14 +12,5 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(errorResponse, ex.getHttpStatus());
     }
 
-    @Value
-    private static class ErrorResponse {
-        int status;
-        String message;
-
-        public ErrorResponse(int status, String message) {
-            this.status = status;
-            this.message = message;
-        }
-    }
+    private record ErrorResponse(int status, String message) {}
 }
