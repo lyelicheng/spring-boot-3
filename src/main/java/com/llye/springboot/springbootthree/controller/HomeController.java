90e5135d0c8b2570efcf11c949533e9fb3351c54
joycelye <joycelye1991@gmail.com>
2023-08-06 16:55:43 +0800

    upgrade the Micrometer dependency version to match the version specified in the spring-boot-starter-parent

--- src/main/java/com/llye/springboot/springbootthree/controller/HomeController.java
+++ src/main/java/com/llye/springboot/springbootthree/controller/HomeController.java
@@ -1,15 +1,19 @@
 package com.llye.springboot.springbootthree.controller;
 
 import io.micrometer.observation.annotation.Observed;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
+    private static final Logger LOG = LoggerFactory.getLogger(HomeController.class);
 
     @Observed(name = "homeController")
     @GetMapping("/")
-    public String home() throws InterruptedException {
+    public String home() {
+        LOG.info("Verify if micrometer tracing is functioning correctly.");
         return "Spring Boot Three API is up!";
     }
 }
